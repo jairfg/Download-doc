8b0f2cf8c9f68b306206efaf96e4180ad9bfea74
JairKevinFG <jkfloresg@uni.pe>
2020-12-01 19:14:23 -0500

    terminado slideshare issuu

--- main.py
+++ main.py
@@ -5,30 +5,42 @@ import os
 import re
 import img2pdf
 import wget
-
+import shutil
 
 def main(url):
-    images = issuu(url)
-    dowloadImg(images)
-#  dowload(images)
+
+    if 'issuu' in url:
+        contenido = issuu(url)
+    elif 'slideshare' in url:
+        contenido = slideshare(url)
+    return dowload(contenido)
+
 def slideshare(url):
+    contenido = {}
     images = []
     try:
         response = requests.get(url)
         if response.status_code == 200:
             s = BeautifulSoup(response.text, 'html.parser')
+            title = s.find('h1',attrs={'class': 'slideshow-title-text'}).span.get_text()
+            title = title.strip()
+            print(title)
             pages = s.findAll('section', attrs={'class': 'slide'})
             for page in pages:
                 page = page.img.get('data-full')
                 images.append(page)
+            contenido['title'] = title
+            contenido['link_pages'] = images
         else:
             print('No se pudo obtener el documento', url)
     except Exception as e:
         print(f'Error {e}')
-    return images
+    return contenido
 
 
 def issuu(url):
+    contenido = {}
+    images = []
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -36,53 +48,56 @@ def issuu(url):
             title = s.find('meta',attrs={
                 'property' : 'og:title'
             }).get('content')
+            print(title)
             imagen_string = s.find('meta',attrs={
                 'property' : 'og:image'
             }).get('content')
-            return issuu_images(imagen_string)
+            imglink2 = imagen_string.replace('1.jpg', '')
+            while True:
+                imglink = imglink2 + str(len(images) + 1) + ".jpg"
+                response = requests.get(imglink)
+                if response.status_code == 403:
+                    break
+                images.append(imglink)
+            contenido['title'] = title
+            contenido['link_pages'] = images
+        else:
+            print('No se pudo obtener el documento', url)
     except Exception as e:
         print(f'Error {e}')
+    return contenido
 
-def issuu_images(imagen_string):
-    images = []
-    imglink2 = imagen_string.replace('1.jpg', '')
-    i = 0
-    while True:
-        i = i +1
-        imglink = imglink2 + str(i) + ".jpg"
-        response = requests.get(imglink)
-        if response.status_code == 403:
-            break
-        images.append(imglink)
-    return images
+def dowload(contenido):
+    images,title = contenido['link_pages'] , contenido['title']
 
-def dowloadImg(images):
-    os.mkdir('dir1')
+    if os.path.isdir(f'./{title}/'):
+        shutil.rmtree(f'{title}')
+
+    os.mkdir(f'{title}')
+    os.mkdir(f'{title}/imagenes')
     for i, image in enumerate(images):
         try:
             img_req = requests.get(image)
-            nombre = f'page{i+1}.jpg'
+            name = f'page{i+1}.jpg'
             if img_req.status_code == 200:
                 print(f'Page {i+1}  : {image}')
-                with open(f'dir1/{nombre}','wb') as handler:
+                with open(f'{title}/imagenes/{name}','wb') as handler:
                     handler.write(img_req.content)
-
-                with open('dir1/urls.txt','a') as handler:
+                with open(f'{title}/urls.txt','a') as handler:
                     handler.write(image + '\n')
             else:
-                print("no se pudo")
-        except:
-            print('No se pudo obtener la imagen')
+                print("No se pudo obtener la pagina")
+        except Exception as e:
+            print(f'Error {e}')
 
-    imagenes_jpg = ['dir1/' + archivo for archivo in os.listdir('dir1/') if archivo.endswith(".jpg")]
-    print(imagenes_jpg)
+    imagenes_jpg = [f'{title}/imagenes/' + archivo for archivo in os.listdir(f'{title}/imagenes/') if archivo.endswith(".jpg")]
     imagenes_jpg.sort(key=lambda x: os.path.getmtime(x))
-    with open("dir1/documento.pdf", "wb") as documento:
+    with open(f'{title}/{title}.pdf', "wb") as documento:
         documento.write(img2pdf.convert(imagenes_jpg))
 
 
 
 
 if __name__ == "__main__":
-    url = 'https://issuu.com/edifaua/docs/catalogo_faua-uni_-20.09'
+    url = input('url: ').strip()
     main(url)
