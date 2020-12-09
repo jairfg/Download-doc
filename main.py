947e297107ce123b5fdf9e58d94551c8767dadf8
JairKevinFG <jkfloresg@uni.pe>
2020-12-08 22:02:34 -0500

    scribd avance

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
@@ -13,7 +15,9 @@ def main(url):
         contenido = issuu(url)
     elif 'slideshare' in url:
         contenido = slideshare(url)
-    return dowload(contenido)
+    elif 'scribd' in url:
+        contenido = scribd(url)
+   # return dowload(contenido)
 
 def slideshare(url):
     contenido = {}
@@ -66,6 +70,47 @@ def issuu(url):
     except Exception as e:
         print(f'Error {e}')
     return contenido
+# dowloand document
+
+def scribd(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            s = BeautifulSoup(response.text, 'lxml')
+            title = s.find("h1").get_text().strip()
+            scripts = s.find_all("script", type="text/javascript");
+            jsonp_urls = []
+            for script in scripts:
+                for content in script:
+                    inicio_url = content.find("https://")
+                    final_url = content.find(".jsonp")
+                    if inicio_url != -1 and final_url != -1:
+                        jsonp = content[inicio_url: final_url + 6]
+                        jsonp_urls.append(jsonp)
+
+            print(f'Extrayendo documento : {title}')
+            for url in jsonp_urls:
+                extract_text(url,title)
+        else:
+            print('No se pudo obtener el documento', url)
+    except Exception as e:
+        print(f'Error {e}')
+
+
+
+def extract_text(url,title):
+    print(url)
+    response = requests.get(url).text
+    page_no = response[11:13]
+    if ('_' in page_no):
+        page_no  = page_no.replace("_","")
+    response_head = response.replace("window.page" + page_no + '_callback(["', "").replace("\\n", "").replace("\\", "").replace('"]);', "").replace("orig","src")
+    print(response_head)
+    s = BeautifulSoup(response_head, "html.parser")
+    s_texts = s.find_all("span", attrs={'class': 'a'})
+
+
+
 
 def dowload(contenido):
     images,title = contenido['link_pages'] , contenido['title']
@@ -99,5 +144,5 @@ def dowload(contenido):
 
 
 if __name__ == "__main__":
-    url = input('url: ').strip()
+    url = 'https://www.scribd.com/document/97749846/Laboratorio-de-Materiales-Fic-uni'
     main(url)
